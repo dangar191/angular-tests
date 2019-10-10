4e8b49101a1661adc12e9d0bf17f3e99df9b3366
dangar191 <blancodaniel2008@gmail.com>
2019-10-10 10:05:37 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,55 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
+  plataforma: string;
+  valor: string;
+  codigo: string;
+  campo: string;
+  etiqueta: string;
 
-  public plataformaSelec: string;
 
   endpoints = [
     {
-      plataforma: 'Nortel',
-      valor: '1',
-      codigo: '1',
+      plataforma: "Nortel",
+      valor: "1",
+      codigo: "1",
       campos: [
         {
-          campo: 'X_VLAN',
-          etiqueta: 'VLAN/ID'
+          campo: "X_VLAN",
+          etiqueta: "VLAN/ID"
         }
       ]
     },
     {
-      plataforma: 'Alcatel',
-      valor: '1',
-      codigo: '2',
+      plataforma: "Alcatel",
+      valor: "1",
+      codigo: "2",
       campos: [
         {
-          campo: 'Y_VLAN2',
-          etiqueta: 'VLAN/ID2'
+          campo: "Y_VLAN2",
+          etiqueta: "VLAN/ID2"
         }
       ]
     }
   ];
 
-    
-  ngOnInit () {
-  }
+  ngOnInit() {
 
+  }
 
   submitPrueba() {
-  
-  }
-
-
-
-  plataformaSeleccionada(valorSelect: any) {
-    this.valorSelect(valorSelect);
+    this.endpoints.forEach(res => {
+      if (this.plataforma === res.codigo) {
+        console.log(res.plataforma);
+      }
+    });
   }
 
   valorSelect(valorSelect: any) {
-    console.log('el valor obtenido del select es: ' + valorSelect);
+    console.log("el valor obtenido del select es: " + valorSelect);
   }
 }
